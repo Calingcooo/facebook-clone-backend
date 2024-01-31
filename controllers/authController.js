b6e1eaa1397a124bcd4e480bdf9496b17985b1d8
Calingcooo <calingco.je@gmail.com>
2024-01-31 15:37:24 +0800

    done auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,29 @@
+const jwt = require("jsonwebtoken");
+
 module.exports = {
   handleLogin: async (req, res) => {
     try {
       const { email, password } = req.body;
 
-      console.log(email);
-
-      if (email === "boyangat@gmail.com") {
-        return res.status(200).json({ message: "Official tanga" });
+      if (email != "boyangat@gmail.com") {
+        return res.status(400).json({ message: "Pasensya, hindi ka tanga." });
       }
+
+      const payload = {
+        email: email,
+      };
+
+      const session = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "15m",
+      });
+
+      res.cookie("session", session, {
+        sameSite: "lax",
+        // secure: true,
+        maxAge: 24 * 60 * 60 * 1000, // 24 hours in milliseconds
+      });
+
+      return res.status(200).json({ message: "Official tanga!" });
     } catch (error) {
       console.error(error);
       return res
